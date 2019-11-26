e80254896f18790d26198338d76b42097a158376
Paulo Henrique <desenvolver.paulo98@gmail.com>
2019-11-26 18:34:53 +0000

    fazendo o implementar falta terminar

---
+++ PrjHospital/implementar.ts
@@ -0,0 +1,24 @@
+namespace hospital{
+    let hospital = new Hospital();
+    hospital.setNome("Santa maria");
+    
+
+    let paciente = new Paciente();
+    paciente.setNome("Paulo");
+    paciente.setCpf("123.456.789-10")
+    paciente.setCodPaciente(5);
+
+    let paciente2 = new Paciente();
+    paciente.setNome("Victoria");
+    paciente.setCpf("109.876.543-21")
+    paciente.setCodPaciente(7);
+
+    let paciente3 = new Paciente();
+    paciente.setNome("Bartholomeu");
+    paciente.setCpf("333.666.333-99")
+    paciente.setCodPaciente(6);
+
+    let medico = new Medico();
+    //TERMINAR...
+
+}
